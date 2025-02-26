528661b7c7ead0cad593a3ab0ea784be3ef21e4e
nqVe <jakub.poprawa95@gmail.com>
2025-02-26 21:34:45 +0100

    scoreboard.py

--- Snake game day 20/scoreboard.py
+++ Snake game day 20/scoreboard.py
@@ -7,7 +7,6 @@ class Scoreboard(Turtle):
         self.score = 0
         with open("highest_score.txt") as file:
             self.highest_score = int(file.read())
-            # self.highest_score = int()
         self.penup()
         self.ht()
         self.color("white")
@@ -15,18 +14,15 @@ class Scoreboard(Turtle):
         self.printing_score()
 
     def update_score(self):
-        """Update scoreboard"""
         self.clear()
         self.score += 1
         self.printing_score()
 
     def clear_score(self):
-        """Update scoreboard"""
         self.clear()
         self.printing_score()
 
     def printing_score(self):
-        """Print scoreboard"""
         self.write(f"Your Score = {self.score} Highest Score: {self.highest_score}", align="center",
                    font=("Arial", 14, "normal"))
 
@@ -39,14 +35,3 @@ class Scoreboard(Turtle):
                 file.read()
         self.score = 0
         self.clear_score()
-    # def game_over_hit_snake(self):
-    #     """Print end of game while hitting snake body"""
-    #     self.home()
-    #     self.color("white")
-    #     self.write("You hit your own tail!! Game Over", align="center", font=("Arial", 16, "normal"))
-    #
-    # def game_over_hit_wall(self):
-    #     """Print end of game while hitting wall"""
-    #     self.home()
-    #     self.color("white")
-    #     self.write("You hit the wall!! Game Over", align="center", font=("Arial", 16, "normal"))
